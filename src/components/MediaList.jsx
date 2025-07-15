134520e85c9a1b4fd1e345606fdfcf85af706d46
Adriano <adrianosenny@gmail.com>
2025-07-15 16:13:21 -0300

    ✨ feat: Adicionar componente MediaList com exibição de mídias em carrossel

---
+++ src/components/MediaList.jsx
@@ -0,0 +1,31 @@
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { MediaItem } from "./MediaItem";
+
+export const MediaList = ({ title, medias }) => {
+  return (
+    <div className="w-full bg-transparent pl-14 relative -top-28">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={6}
+        slidesPerView={6.5}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        <h3 className="text-slate-50 font-bold absolute top-0">{title}</h3>
+
+        {medias.map((media) => {
+          return (
+            <SwiperSlide>
+              <MediaItem media={media} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
