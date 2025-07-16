4a6bf3ace88bbd85961c54e3f3e008759b9af8f5
Adriano <adrianosenny@gmail.com>
2025-07-16 15:43:51 -0300

    ✨ feat: Adicionar estilização e imagem de fundo ao componente MediaItem

--- src/components/MediaItem.jsx
+++ src/components/MediaItem.jsx
@@ -1,12 +1,14 @@
 export const MediaItem = ({ media }) => {
   return (
-    <div className="relative">
+    <div
+      className="relative h-full bg-cover rounded-md"
+      style={{ backgroundImage: `url(${media.image})` }}
+    >
       <img
         className="w-[10px] absolute left-3 top-2"
         src="/logo_sm.svg"
         alt="Logo Listo menor, apenas o L"
       />
-      <img className="h-full rounded-sm" src="/totoro.png" alt="" />
     </div>
   );
 };
